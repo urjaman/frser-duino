4442575aeaa820684ec99eca08e282e6357377c0
Urja Rannikko <urjaman@gmail.com>
2013-10-09 07:32:53 +0200

    Optimize lpc_send_addr
    As a note, i calculated that the previous iterative shift
    algo took atleast 560 cycles just shifting bits while sending
    an address (that is 46 2/3 microseconds => ~21kHz ...)
    Now we have 3 cycles of what could be classified as shifts...

--- lpc.c
+++ lpc.c
@@ -19,6 +19,7 @@
 #include "nibble.h"
 #include "lpc.h"
 #include "parallel.h"
+#include "typeu.h"
 
 #define LPC_START 0b0000
 #define LPC_CYCTYPE_READ 0b0100
@@ -46,12 +47,38 @@ static void lpc_nibble_write(uint8_t value)
 	clocked_nibble_write(value);
 }
 
+#define swap(x) do { asm volatile("swap %0" : "=r" (x) : "0" (x)); } while(0)
+
 static void lpc_send_addr(uint32_t addr)
 {
+#if 0
 	int8_t i;
 	addr |= LPC_BL_ADDR;
 	for (i = 28; i >= 0; i -= 4)
 		lpc_nibble_write((addr >> i) & 0xf);
+#else
+	uint8_t tmp;
+	u32_u a;
+	a.l = addr;
+	/* NOTE: revise this if LPC_BL_ADDR changes. */
+	lpc_nibble_write(0xF);
+	lpc_nibble_write(0xF);
+	tmp = a.b[2];
+	swap(tmp);
+	lpc_nibble_write(tmp);
+	//swap(a.b[2]);
+	lpc_nibble_write(a.b[2]);
+	tmp = a.b[1];
+	swap(tmp);
+	lpc_nibble_write(tmp);
+	//swap(a.b[1]);
+	lpc_nibble_write(a.b[1]);
+	tmp = a.b[0];
+	swap(tmp);
+	lpc_nibble_write(tmp);
+//	swap(a.b[0]);
+	lpc_nibble_write(a.b[0]);
+#endif
 }
 
 int lpc_read_address(uint32_t addr)

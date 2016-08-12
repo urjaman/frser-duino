20352b5d9c6530f52ccffd9acb07159c960a7fe5
Urja Rannikko <urjaman@gmail.com>
2016-08-12 15:20:02 +0300

    Make spihw.h understand both the old duinos and the megas... and error so one knows to fix stuff if necessary

--- spihw.h
+++ spihw.h
@@ -1,7 +1,8 @@
 /* Define what our hardware is. */
 #include "spihw_avrspi.h"
 
-
+#if (defined __AVR_ATmega328P__)||(defined __AVR_ATmega328__)||(defined __AVR_ATmega168P__)||(defined __AVR_ATmega168__)||(__AVR_ATmega88P__)||(__AVR_ATmega88__)||(defined __AVR_ATmega48P__)||(defined __AVR_ATmega48__)
+/* The usual duinos ... */
 #define SPI_PORT	PORTB
 #define SCK		PORTB5		/* port 13 */
 #define MISO		PORTB4		/* port 12 */
@@ -16,6 +17,22 @@
 #else
 #define spi_select() do { SPI_PORT &= ~(1<<SS); } while(0)
 #define spi_deselect() do { SPI_PORT |= (1<<SS); } while(0)
+#endif
+
+#elif (defined __AVR_ATmega1280__)||(defined __AVR_ATmega1281__)||(defined __AVR_ATmega2560__)||(defined __AVR_ATmega2561__)
+/* The Megas ... */
+
+#define SPI_PORT	PORTB
+#define DDR_SPI		DDRB
+#define SCK		PORTB1
+#define MISO		PORTB3
+#define MOSI 		PORTB2
+#define SS		PORTB0
+#define spi_select() do { SPI_PORT &= ~(1<<SS); } while(0)
+#define spi_deselect() do { SPI_PORT |= (1<<SS); } while(0)
+
+#else
+#error define your SPI hardware pins
 #endif
 
 
